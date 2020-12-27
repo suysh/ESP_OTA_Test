2124d7ecfcf7ad5356dca32c14cd9119d7ec4888
Hans <43004004+suysh@users.noreply.github.com>
2020-12-27 14:57:16 +0100

    First upload

---
+++ main.py
@@ -0,0 +1,28 @@
+def connectToWifiAndUpdate():
+    import machine, network, gc, app.secrets as secrets
+    print('Memory free', gc.mem_free())
+
+    from lib.ota_updater import OTAUpdater
+
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+        sta_if.connect(secrets.WIFI_SSID, secrets.WIFI_PASSWORD)
+        while not sta_if.isconnected():
+            pass
+    print('network config:', sta_if.ifconfig())
+    otaUpdater = OTAUpdater('https://github.com/suysh/ESP_OTA_Test', main_dir='app', secrets_file="secrets.py")
+    hasUpdated = otaUpdater.install_update_if_available()
+    if hasUpdated:
+        machine.reset()
+    else:
+        del(otaUpdater)
+        gc.collect()
+
+def startApp():
+    import app.start
+
+
+connectToWifiAndUpdate()
+startApp()
